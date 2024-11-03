fcc352523e781066a35fbd0821339628bb6912b8
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-03 14:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=1; i<str.length-1; i++) {
+		//console.log(str[i], str[i+1]);
+		if(str[i]!==str[i+1] && str[i]!==str[i-1]) {
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
