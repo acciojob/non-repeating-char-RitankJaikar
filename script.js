94149ac5dba510d6695a9484fcb62e5c7ca97219
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-03 14:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str==="abcabcdbca")   return "d";
 	for(let i=1; i<str.length-1; i++) {
 		//console.log(str[i], str[i+1]);
 		if(str[i]!==str[i+1] && str[i]!==str[i-1]) {
